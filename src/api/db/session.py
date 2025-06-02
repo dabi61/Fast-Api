4348b0f1e73c635e783415507940d013ff62bb36
dabi61 <daodinhbinh612004@gmail.com>
2025-06-02 11:57:48 +0700

    update

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -1,7 +1,7 @@
 import sqlmodel
 from .config import DATABASE_URL
 from sqlmodel import SQLModel, Session
-
+import timescaledb
 if DATABASE_URL == "":
     raise NotImplementedError("DATABASE_URL needs to be set")
 
@@ -12,6 +12,7 @@ engine = sqlmodel.create_engine(DATABASE_URL)
 def init_db():
     print("creating database")
     SQLModel.metadata.create_all(engine)
+
 
 
 def get_session():
