6584fe5c06062c616da969958310bb4ac43b895c
dabi61 <daodinhbinh612004@gmail.com>
2025-06-02 10:35:44 +0700

    update deploy

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -10,6 +10,7 @@ from .model import (
     EventListScheme,
     EventCreateSchema,
     EventUploadSchema,
+    get_utc_now
     )
 router = APIRouter()
 from api.db.config import DATABASE_URL
@@ -17,7 +18,7 @@ from api.db.config import DATABASE_URL
 #List View
 @router.get("/", response_model=EventListScheme)
 def read_events(session: Session = Depends(get_session)):
-    query = select(EventModel)
+    query = select(EventModel).order_by(EventModel.update_at.desc()).limit(10)
     results = session.exec(query).all()
     return {
         "results": results,
@@ -48,6 +49,18 @@ def get_event(event_id: int, session: Session = Depends(get_session)):
     # a single row
     return results
 
+@router.delete("{event_id}", response_model=EventModel)
+def delete_event(event_id: int, session: Session = Depends(get_session)):
+    query = select(EventModel).where(EventModel.id == event_id)
+    results = session.exec(query).first()
+    if not results:
+        raise HTTPException(status_code=404, detail="Event Not Found")
+    # a single row
+    session.delete(results) #Delete
+    session.commit() #commit
+    return results
+
+
 
 @router.put("/{event_id}", response_model=EventModel)
 def update_event(
@@ -64,6 +77,8 @@ def update_event(
         # if k == 'id':  #Mo len de tranh cap nhat field id Nhung minh thiet ke payload kia thi khong can thiet
         #     continue
         setattr(obj, k, v)
+    obj.update_at = get_utc_now()
+        # obj.create_at =
 
     session.add(obj)
     session.commit()
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from api.events import router as event_router
 from contextlib import asynccontextmanager
 from api.db.session import init_db
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 @asynccontextmanager
 async def lifespan( app:FastAPI):
@@ -12,6 +14,13 @@ async def lifespan( app:FastAPI):
     #clean up
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_orgins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(event_router, prefix='/api/events') #/api/events
 
 
